b6207598c16f7316adb5a36f313f2db6904884ea
Joey <josephrobertlopez@gmail.com>
2025-02-23 15:32:55 -0600

    add stack probs

---
+++ stack/stack.py
@@ -0,0 +1,28 @@
+class Solution:
+    # https://leetcode.com/problems/valid-parentheses/
+    def isValid(self, s: str) -> bool:
+        pass
+
+    # https://leetcode.com/problems/min-stack/
+    class MinStack:
+        pass
+    
+    # https://leetcode.com/problems/evaluate-reverse-polish-notation/
+    def evalRPN(self, tokens: list[str]) -> int:
+        pass
+
+    # https://leetcode.com/problems/generate-parentheses/
+    def generateParenthesis(self, n: int) -> list[str]:
+        pass
+
+    # https://leetcode.com/problems/daily-temperatures/
+    def dailyTemperatures(self, temperatures: list[int]) -> list[int]:
+        pass
+
+    # https://leetcode.com/problems/car-fleet/
+    def carFleet(self, target: int, position: list[int], speed: list[int]) -> int:
+        pass
+
+    # https://leetcode.com/problems/largest-rectangle-in-histogram/
+    def largestRectangleArea(self, heights: list[int]) -> int:
+        pass
